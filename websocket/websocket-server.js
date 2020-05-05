e7c9156ebaabd5ccf83f03e7b871c63774edd86b
Elias HAMADOUCHE <elinole94@gmail.com>
2020-05-05 17:14:01 +0200

    Websocket

--- websocket/websocket-server.js
+++ websocket/websocket-server.js
@@ -1,9 +1,20 @@
 const WebSocket = require('ws')
 const webSocketServer = new WebSocket.Server({ port: 3003 });
   
+// latest 100 messages
+var history = [ ];
+
 webSocketServer.on('connection', webSocket => {
+    // send back chat history
+    if (history.length > 0) {
+        webSocketServer.sendUTF(
+            JSON.stringify({ type: 'history', data: history} ));
+    }
     webSocket.onmessage = messageEvent => {
+        // we want to keep history of all sent messages
         const message = messageEvent.data;
+        history.push(message);
+        history = history.slice(-100);
         webSocketServer.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(message);
